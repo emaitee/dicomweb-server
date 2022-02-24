067570ba67b8f64318ea47b46c1002d60b4800e1
Mustapha Issa Toyin <31075233+emaitee@users.noreply.github.com>
2022-02-24 04:38:37 +0100

    save changes

--- config/development.js
+++ config/development.js
@@ -1,8 +1,8 @@
 module.exports = {
   env: 'development',
-  dbServer: process.env.DB_SERVER || 'http://localhost',
-  db: process.env.DB_NAME || 'chronicle',
-  dbPort: process.env.DB_PORT || 5984,
+  dbServer: process.env.DB_SERVER || 'https://apikey-v2-hdg7q7sdpzex2fpt9c7zpvisgeq5gv6a3b67068es47:2c266d416856b89636469e59771a3fb2@c13e559c-61e5-4443-ae88-f778dc3507c6-bluemix.cloudantnosqldb.appdomain.cloud',
+  db: process.env.DB_NAME || 'optimum-dicom',
+  dbPort: process.env.DB_PORT || 443,
   prefix: process.env.PREFIX || '',
   auth: process.env.AUTH || 'none',
   logger: process.env.LOGGER || true,
